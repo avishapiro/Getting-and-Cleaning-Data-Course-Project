a16e140e082a4e80a3084b275ecba2b91ae995fc
Avi Shapiro <avi.m.shapiro@gmail.com>
2015-09-27 08:54:51 -0400

    Updates data tidying script and resulting tidy data

--- run_analysis.R
+++ run_analysis.R
@@ -7,11 +7,14 @@
 # 3. Uses descriptive activity names to name the activities in the data set
 # 4. Appropriately labels the data set with descriptive variable names.
 # 5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+## -------------------------------------------------
 
 
+## Step 0 ------------------------------------------------- ##
 # Download data if necessary (set working directory first!!)
+print("Checking for raw data...")
 if (!file.exists("./UCI\ HAR\ Dataset")){
-    print("No data directory exists.")
+    print("No raw data directory exists.")
     if (!file.exists("getdata-projectfiles-UCI\ HAR\ Dataset.zip")) {
         print("Downloading data zipfile.")
         fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
@@ -22,8 +25,9 @@ if (!file.exists("./UCI\ HAR\ Dataset")){
     print("Uncompressing data file.")
     unzip("getdata-projectfiles-UCI\ HAR\ Dataset.zip")
 }
+print("Raw data is ready for processing.")
 
-## Step 1
+## Step 1 ------------------------------------------------- ##
 # There are 6 datasets of interest. We merge them in 5 steps.
 X_train <- read.table("UCI HAR Dataset/train/X_train.txt", colClasses = c("numeric"))
 X_test <- read.table("UCI HAR Dataset/test/X_test.txt", colClasses = c("numeric"))
@@ -45,7 +49,9 @@ merged_df <- cbind(X, Y, subject)
 # Remove unneeded raw data frames from memory
 rm(X_train, Y_train, subject_train, X_test, Y_test, subject_test, X, Y, subject)
 
-## Step 2
+print("Step 1 finished")
+
+## Step 2 ------------------------------------------------- ##
 # Extract features involving mean or std
 features <- read.table("UCI HAR Dataset/features.txt",
                        colClasses = c("numeric", "character"))
@@ -57,7 +63,9 @@ meanstd_df <- merged_df[,indices]
 # Remove unneeded data.frame
 rm(merged_df)
 
-## Step 3
+print("Step 2 finished")
+
+## Step 3 ------------------------------------------------- ##
 # Replace Y column integers with strings from activity_labels.txt
 
 f <- function(d) {
@@ -75,17 +83,38 @@ desc_activity_df[, "V1.1"] <- as.factor(sapply(meanstd_df[, "V1.1"], f))
 # Remove unneeded data.frame
 rm(meanstd_df)
 
-## Step 4
-# Label the features
-names(desc_activity_df) <- c(features[indices,2], "activity", "subject")
+print("Step 3 finished")
 
+## Step 4 ------------------------------------------------- ##
+# Label the features with proper, descriptive R names
+feature_names <- gsub("\\(\\)", "", features[indices, 2])
+feature_names <- gsub("-", "", feature_names)
+feature_names <- gsub("mean", "Mean", feature_names)
+feature_names <- gsub("std", "Std", feature_names)
+feature_names <- gsub("Acc", "Accelerometer", feature_names)
+feature_names <- gsub("Gyro", "Gyrometer", feature_names)
+feature_names <- gsub("Mag", "Magnitude", feature_names)
+names(desc_activity_df) <- c(feature_names, "activity", "subject")
+clean_df <- desc_activity_df
 
-## Step 5
+# Remove unneeded data.frame
+rm(desc_activity_df, features)
+
+print("Step 4 finished")
+
+## Step 5 ------------------------------------------------- ##
 # Create tidy summary data set
-s <- split(desc_activity_df[, !names(desc_activity_df) %in% c("activity", "subject")],
-           list(desc_activity_df$activity, desc_activity_df$subject))
-tidy_df <- t(data.frame(lapply(s, colMeans)))
+s <- split(clean_df[, !names(clean_df) %in% c("activity", "subject")],
+           list(clean_df$activity, clean_df$subject))
+tidy_df <- data.frame(t(data.frame(lapply(s, colMeans))))
+
+# Remove unneeded data.frame
+rm(s)
 
 # Save tidy data set to file
 write.table(tidy_df, file = "tidy_data.txt", row.names = FALSE)
 
+print("Step 5 finished")
+print("Tidy data set has been written to tidy_data.txt.")
+
+## End of script ------------------------------------------------- ##
